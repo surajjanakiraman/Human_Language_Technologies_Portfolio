dc494a1bae01f7ffc3d18e975e692ba68a9afa6a
surajjanakiraman <sxj170022@utdallas.edu>
2022-09-04 22:36:32 -0500

    Add Homework 1 files via Upload

---
+++ sxj170022PortfolioAssignment1/homework1_config.py
@@ -0,0 +1,5 @@
+#Configuration file
+#Store file names so that the file names DO NOT HAVE TO BE HARDCODED EVERYTIME THEY NEED TO BE REFERENCED
+input_file = 'data/data.csv'
+output_file = 'output.txt'
+pickle_file = 'dict.p'
